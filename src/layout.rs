94dfff73dd8ca13ef1901f3b047f8650c552a338
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2021-02-14 22:13:06 +0900

    layout::layout_tree

--- src/layout.rs
+++ src/layout.rs
@@ -260,6 +260,20 @@ impl<'a> LayoutBox<'a> {
     }
 }
 
+/// Transform a style tree into a layout tree.
+pub fn layout_tree<'a>(
+    node: &'a StyledNode<'a>,
+    mut containing_block: Dimensions,
+) -> LayoutBox<'a> {
+    // The layout algorithm expects the container height to start at 0.
+    // TODO: Save the initial containing block height, for calculating percent heights.
+    containing_block.content.height = 0.0;
+
+    let mut root_box = build_layout_tree(node);
+    root_box.layout(containing_block);
+    root_box
+}
+
 /// Build the tree of LayoutBoxes, but don't perform any layout calculations yet.
 fn build_layout_tree<'a>(style_node: &'a StyledNode<'a>) -> LayoutBox<'a> {
     // Create the root box.
